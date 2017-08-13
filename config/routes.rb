0e71e3a5de2f8b273ebbc554003bd5134c4d201c
Gorla <gorla.nagender@gmail.com>
2017-08-13 09:24:20 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+
+  # Define routes for Pages.
+  get 'home' => 'pages#home'
+  get 'profile' => 'pages#profile'
+  get 'explore' => 'pages#explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
