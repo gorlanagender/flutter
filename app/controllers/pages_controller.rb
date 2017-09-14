61b194a9771d8e3b4366d7131a0daf6c5a88841e
Gorla <gorla.nagender@gmail.com>
2017-09-13 21:04:24 -0700

    remove unnecessary files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,13 @@ class PagesController < ApplicationController
   end
 
   def home
-    @posts = present(data: post_service.get_posts, presenter: PostsPresenter)
+    @posts = post_service.get_posts
     @new_post = post_service.build_post
   end
 
   def profile
     @user = user_service.get_profile(name: params[:id])
-    @posts = present(data: post_service.get_all_posts(user_id: @user.id),
-                     presenter: PostsPresenter)
+    @posts = post_service.get_all_posts(user_id: @user.id)
     @new_post = post_service.build_post
     @users = user_service.get_top_users
   rescue Exceptions::UserExceptions::RecordNotFound
