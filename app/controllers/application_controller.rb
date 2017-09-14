b3b2cf7afe662034f40b2e4b2259a33ecac0cdd1
Gorla <gorla.nagender@gmail.com>
2017-09-14 16:41:55 -0700

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(_)
-    UserMailer.new.sent_mail(user: current_user)
+    UserMailer.sent_mail(user: current_user).deliver
     home_path
   end
 
